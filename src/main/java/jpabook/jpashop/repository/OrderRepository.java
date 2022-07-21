ede45b711bcce153397e23aa79f5855a46406337
kjkim <apio1130@naver.com>
2022-07-22 01:25:38 +0900

    test(order): 주문 기능 테스트 작성

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -10,7 +10,7 @@ import javax.persistence.EntityManager;
 @RequiredArgsConstructor
 public class OrderRepository {
 
-    private EntityManager em;
+    private final EntityManager em;
 
     public void save(Order order) {
         em.persist(order);
