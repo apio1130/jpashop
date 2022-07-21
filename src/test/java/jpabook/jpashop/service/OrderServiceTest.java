bc01abddf88ffe352699c86a1bc43d1bffdd6ea8
kjkim <apio1130@naver.com>
2022-07-21 23:47:17 +0900

    test(order): 주문 기능 테스트 목록 작성

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,32 @@
+package jpabook.jpashop.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class OrderServiceTest {
+
+
+    @Test
+    void 상품주문() throws Exception {
+
+    }
+
+    @Test
+    void 주문취소() throws Exception {
+
+    }
+
+    @Test
+    void 상품주문_재고수초과() throws Exception {
+
+    }
+
+}
